d7b56dfb0a4a89f0a6c42cf0cbb738cb482d5e43
Mario Reder <mario.reder@pm.me>
2025-10-13 12:18:44 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Crack open some Leverage",
   "VITE_HAS_PRIMARY_LOGO": "false",
   "VITE_HAS_SECONDARY_LOGO": "false",
-  "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Rewards,Leaderboard,Markets,Vaults",
+  "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Rewards,Leaderboard,Markets",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
