a2b8635b703ccdee57975538cfae86bd4dc6dfa4
Mario Reder <mario.reder@pm.me>
2025-10-16 14:32:21 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,14 +25,14 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "PERPSODA",
   "VITE_SEO_SITE_DESCRIPTION": "Crack open some Leverage",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/perps-5322/",
+  "VITE_SEO_SITE_URL": "https://perpsoda.fun",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
   "VITE_SEO_THEME_COLOR": "#000000",
   "VITE_SEO_KEYWORDS": "",
   "VITE_AVAILABLE_LANGUAGES": "en,zh,ja,es,ko,vi,de,fr,ru,id,tr,it,pt,uk,pl,nl",
-  "VITE_USE_CUSTOM_PNL_POSTERS": "false",
-  "VITE_CUSTOM_PNL_POSTER_COUNT": "0",
+  "VITE_USE_CUSTOM_PNL_POSTERS": "true",
+  "VITE_CUSTOM_PNL_POSTER_COUNT": "2",
   "VITE_TRADING_VIEW_COLOR_CONFIG": ""
 };
